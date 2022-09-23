6fc6cb688ec4085db403e4c79c959e2278b80a4e
Marcos Cunha <marcos.cunha@ffit.com.br>
2022-09-23 07:52:39 -0300

    Create Spring project

---
+++ src/main/java/br/com/rbmf_2022/inscribed/InscribedApplication.java
@@ -0,0 +1,13 @@
+package br.com.rbmf_2022.inscribed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscribedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscribedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rbmf_2022/inscribed/InscribedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rbmf_2022.inscribed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscribedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
